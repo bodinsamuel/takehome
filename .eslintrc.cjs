8e721f6eab5b7a04a445a698e9d1bdc63ab1e98e
Samuel Bodin <1637651+bodinsamuel@users.noreply.github.com>
2024-01-07 11:54:58 +0100

    initial commit

---
+++ .eslintrc.cjs
@@ -0,0 +1,42 @@
+// eslint-disable-next-line no-undef
+module.exports = {
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/strict',
+    'plugin:@typescript-eslint/stylistic',
+    'plugin:import/errors',
+    'plugin:import/warnings',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'plugin:prettier/recommended',
+  ],
+  parser: '@typescript-eslint/parser',
+  plugins: ['@typescript-eslint', 'import', 'require-extensions', 'vitest'],
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+    ecmaFeatures: {
+      impliedStrict: true,
+      jsx: true,
+    },
+    project: false,
+  },
+
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        arrowParens: 'always',
+        bracketSpacing: true,
+        bracketSameLine: false,
+        printWidth: 80,
+        singleQuote: true,
+        trailingComma: 'es5',
+        useTabs: false,
+        quoteProps: 'as-needed',
+      },
+    ],
+  },
+};
